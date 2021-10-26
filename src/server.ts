fceb18d2e340f4c80f57fa6c6ac7800b5a9f16d1
Torben Feldmann <torben.feldmann1@gmail.com>
2021-10-26 17:11:02 +0200

    add CRUD API

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,38 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+let names = [{ name: 'superman' }, { name: 'batman' }, { name: 'Rotkäpchen' }];
+
+app.get('/users/:name', (_req, res) => {
+  const queryString = _req.params.name;
+  const foundUser = names.find((user) => {
+    if (queryString === user.name) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  if (foundUser) {
+    res.send('wuwhuuuu');
+  } else {
+    res.status(404).send('noooooo');
+  }
+});
+
+app.post('/users/', (req, res) => {
+  res.send(req.body.name);
+});
+
+app.delete('/users/:name', (_req, res) => {
+  names = names.filter((user) => user.name !== _req.params.name);
+  res.send(names);
+});
 
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  res.send(names);
 });
 
 app.listen(port, () => {
